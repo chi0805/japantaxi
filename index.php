d8119a5132a3c9a8ebb90151710ea13918380ce9
Chisato <c23021705@gmail.com>
2020-02-21 08:02:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>japan taxi</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header">
+        <a class="nav-logo" href="#"><img class="nav-logo" src="./images/logo_navy.svg" alt="nav-logo"></a>
+        <div class="global-nav">
+            <div class="global-nav-part">Company</div>
+            <div class="global-nav-part">Services</div>
+            <div class="global-nav-part">News</div>
+            <div class="global-nav-part">Recruit</div>
+            <div class="global-nav-part">Contact</div>
+            <div class="global-nav-part">Blog</div>
+        </div>
+    </header>
+</body>
+</html>
