c4e2bfb811458ae6b54b25bb2e1be6f1e561363d
Chisato <c23021705@gmail.com>
2020-02-21 23:23:07 +0900

    アイキャッチ追加

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>japan taxi</title>
+    <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -10,13 +11,43 @@
     <header class="header">
         <a class="nav-logo" href="#"><img class="nav-logo" src="./images/logo_navy.svg" alt="nav-logo"></a>
         <div class="global-nav">
-            <div class="global-nav-part">Company</div>
-            <div class="global-nav-part">Services</div>
-            <div class="global-nav-part">News</div>
-            <div class="global-nav-part">Recruit</div>
-            <div class="global-nav-part">Contact</div>
-            <div class="global-nav-part">Blog</div>
+            <div class="global-nav-part">
+                <a href="#">
+                    Company
+                </a>
+            </div>
+            <div class="global-nav-part">
+                <a href="#">
+                    Services
+                </a>
+            </div>
+            <div class="global-nav-part">
+                <a href="#">
+                    News
+                </a>
+            </div>
+            <div class="global-nav-part">
+                <a href="#">
+                    Recruit
+                </a>
+            </div>
+            <div class="global-nav-part">
+                <a href="#">
+                    Contact
+                </a>
+            </div>
+            <div class="global-nav-part">
+                <a href="#">
+                    Blog&nbsp&nbsp<img class="blog" src="./images/icon_blank_navy.svg"></img></div>
+                </a>
         </div>
     </header>
+    <section class="eye-catch">
+        <div class="background-image">
+            <img class="eyecatch-logo" src="./images/hero_logo01.svg" alt="introduce">
+            <img class="eyecatch-logo" src="./images/hero_logo02.svg" alt="introduce">
+            <img class="eyecatch-logo" src="./images/hero_logo03.svg" alt="introduce">
+        </div>
+    </section>
 </body>
 </html>
