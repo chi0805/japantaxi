0be241b12fd1d5a06baaf82a59e54ae8e1c1695e
chi0805 <c23021705@gmail.com>
2020-02-22 14:23:23 +0900

    mission追加

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>japan taxi</title>
-    <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -44,10 +44,26 @@
     </header>
     <section class="eye-catch">
         <div class="background-image">
-            <img class="eyecatch-logo" src="./images/hero_logo01.svg" alt="introduce">
-            <img class="eyecatch-logo" src="./images/hero_logo02.svg" alt="introduce">
-            <img class="eyecatch-logo" src="./images/hero_logo03.svg" alt="introduce">
+            <img class="eyecatch-logo logo01" src="./images/hero_logo01.svg" alt="introduce"><br/>
+            <img class="eyecatch-logo logo02" src="./images/hero_logo02.svg" alt="introduce"><br/>
+            <img class="eyecatch-logo logo03" src="./images/hero_logo03.svg" alt="introduce"><br/>
         </div>
     </section>
+    <div class="mission-nav">
+        <a href="#">
+            <p>Our Misiion</p>
+        </a>
+    </div>
+    <section class="mission">
+        <div class="mission-description">
+移動に関わるサービスの提供を通じて、<br/>
+機会を全ての⼈々に対して等しく提供すること。<br/>
+加えて、移動の時間そのものも「コスト」ではなく<br/>
+「機会」に変えていくこと。<br/>
+今現在の移動も、未来の移動でも、喜びに感じる体験を提供し、<br/>
+「⼈を幸せにすること。」を私たちは⽬指しています。<br/>
+        </div>
+    </section>
+
 </body>
 </html>
