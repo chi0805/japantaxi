786dbc38694292c09f0b07eea68260a22ee50a64
chi0805 <c23021705@gmail.com>
2020-02-24 22:13:02 +0900

    サービス追加

--- index.php
+++ index.php
@@ -55,19 +55,53 @@
         </a>
     </div>
     <section class="mission">
-        <div class="mission-description">
+        <div class="mission-main">
+            <div class="mission-description">
 移動に関わるサービスの提供を通じて、<br/>
 機会を全ての⼈々に対して等しく提供すること。<br/>
 加えて、移動の時間そのものも「コスト」ではなく<br/>
 「機会」に変えていくこと。<br/>
 今現在の移動も、未来の移動でも、喜びに感じる体験を提供し、<br/>
 「⼈を幸せにすること。」を私たちは⽬指しています。<br/>
-            <div class="mission-nav">
-                <a href="#">
-                    <p>Our Misiion</p>
-                </a>
+                <div class="mission-nav">
+                    <a href="#">
+                        <p>Our Misiion</p>
+                    </a>
+                </div>
             </div>
         </div>
+    </section>
+    <section class="service">
+        <div class="service-title">
+            <p class="service-title-main">Services</p>
+            <p class="service-title-sub">わたしたちのサービス</p>
+        </div>
+            <div class="service-list">
+                <div class="service-item item-left">
+                    <img class="service-image" src="./images/service_app.jpg" alt="service_app">
+                    <p class="service-item-title">「JapanTaxi」アプリ</p>
+                </div>
+                <div class="service-item item-center">
+                    <img class="service-image" src="./images/service_wallet.jpg" alt="service_wallet">
+                    <p class="service-item-title">JapanTaxi Wallet</p>
+                </div>
+                <div class="service-item item-right">
+                    <img class="service-image" src="./images/service_business.jpg" alt="service_business">
+                    <p class="service-item-title">JapanTaxi BUSINESS</p>
+                </div>
+                <div class="service-item item-left">
+                    <img class="service-image" src="./images/service_tablet-1.jpg" alt="service_tablet-1">
+                    <p class="service-item-title">JapanTaxiタブレット</p>
+                </div>
+                <div class="service-item item-center">
+                    <img class="service-image" src="./images/service_recorder.jpg" alt="service_recorder">
+                    <p class="service-item-title">JapanTaxiドライブレコーダー4</p>
+                </div>
+                <div class="service-item item-right">
+                    <img class="service-image" src="./images/service_dataplatform.jpg" alt="service_dataplatform">
+                    <p class="service-item-title">JapanTaxi Data Platform</p>
+                </div>
+            </div>
     </section>
 </body>
 </html>
